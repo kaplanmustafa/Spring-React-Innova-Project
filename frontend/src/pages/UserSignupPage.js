6d7f5b05ea12a52554815e45f517ae4c918401d0
DESKTOP-0G02NO8\mustafakaplan <mstfakapln@gmail.com>
2021-02-08 20:18:19 +0300

    signup page interactions

--- frontend/src/pages/UserSignupPage.js
+++ frontend/src/pages/UserSignupPage.js
@@ -1,25 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 const UserSignupPage = () => {
+  const [form, setForm] = useState({
+    username: null,
+    fullName: null,
+    password: null,
+    passwordRepeat: null,
+  });
+
+  const onChange = (event) => {
+    const { name, value } = event.target;
+    setForm((previousForm) => ({ ...previousForm, [name]: value }));
+  };
+
   return (
     <div>
       <form>
         <h1>Sign Up</h1>
         <div>
           <label>Username</label>
-          <input />
+          <input name="username" onChange={onChange} />
         </div>
         <div>
           <label>Full Name</label>
-          <input />
+          <input name="fullName" onChange={onChange} />
         </div>
         <div>
           <label>Password</label>
-          <input type="password" />
+          <input name="password" type="password" onChange={onChange} />
         </div>
         <div>
           <label>Password Repeat</label>
-          <input type="password" />
+          <input name="passwordRepeat" type="password" onChange={onChange} />
         </div>
         <button>Sign Up</button>
       </form>
