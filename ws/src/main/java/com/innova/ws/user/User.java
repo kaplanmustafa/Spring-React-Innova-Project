f5867163d9aac826e4598d427933bbc8ce9ebe35
mustafakaplan <mstfakapln@gmail.com>
2021-02-09 14:39:21 +0300

    security configuration for user

--- ws/src/main/java/com/innova/ws/user/User.java
+++ ws/src/main/java/com/innova/ws/user/User.java
@@ -1,15 +1,19 @@
 package com.innova.ws.user;
 
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Collection;
 
-@Entity
 @Data // Auto Getter and Setter
-public class User {
+@Entity
+public class User implements UserDetails{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,4 +24,24 @@ public class User {
     private String fullName;
 
     private String password;
+
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return AuthorityUtils.createAuthorityList("Role_user");
+    }
+
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    public boolean isEnabled() {
+        return true;
+    }
 }
