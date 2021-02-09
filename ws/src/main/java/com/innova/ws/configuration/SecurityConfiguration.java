8a49d158335f66fe318656f8448bff2d22bfbc0b
mustafakaplan <mstfakapln@gmail.com>
2021-02-09 14:29:18 +0300

    refactor password encoder

--- ws/src/main/java/com/innova/ws/configuration/SecurityConfiguration.java
+++ ws/src/main/java/com/innova/ws/configuration/SecurityConfiguration.java
@@ -1,10 +1,13 @@
 package com.innova.ws.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -22,5 +25,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
                 .authorizeRequests().anyRequest().permitAll();
 
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
