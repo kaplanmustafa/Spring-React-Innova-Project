e9f91870b664a5ade542a4c9d077d24131f0e8cc
mustafakaplan <mstfakapln@gmail.com>
2021-02-09 12:42:02 +0300

    hashing password

--- ws/src/main/java/com/innova/ws/user/UserService.java
+++ ws/src/main/java/com/innova/ws/user/UserService.java
@@ -1,17 +1,22 @@
 package com.innova.ws.user;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
     UserRepository userRepository;
+    PasswordEncoder passwordEncoder;
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+        this.passwordEncoder = new BCryptPasswordEncoder();
     }
 
     public void save(User user) {
+        user.setPassword(this.passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
     }
 }
