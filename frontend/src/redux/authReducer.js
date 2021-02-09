ed075fb55b26c9e1dd7a45769db19dfb43def75a
mustafakaplan <mstfakapln@gmail.com>
2021-02-09 17:51:05 +0300

    auth reducer

---
+++ frontend/src/redux/authReducer.js
@@ -0,0 +1,21 @@
+import * as ACTIONS from "./Constants";
+
+const defaultState = {
+  isLoggedIn: false,
+  username: undefined,
+  fullName: undefined,
+  role: undefined,
+};
+
+const authReducer = (state = { ...defaultState }, action) => {
+  if (action.type === ACTIONS.LOGIN_SUCCESS) {
+    return {
+      ...action.payload,
+      isLoggedIn: true,
+    };
+  }
+
+  return state;
+};
+
+export default authReducer;
