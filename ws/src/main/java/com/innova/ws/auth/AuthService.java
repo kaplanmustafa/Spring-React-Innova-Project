9883840d2696a312554a678f9e5436edfd548ce0
mustafakaplan <mstfakapln@gmail.com>
2021-02-10 11:19:41 +0300

    JWT token generation

--- ws/src/main/java/com/innova/ws/auth/AuthService.java
+++ ws/src/main/java/com/innova/ws/auth/AuthService.java
@@ -1,20 +1,38 @@
 package com.innova.ws.auth;
 
+import com.innova.ws.user.User;
 import com.innova.ws.user.UserService;
+import com.innova.ws.user.vm.UserVM;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
 
     UserService userService;
+    PasswordEncoder passwordEncoder;
 
-    public AuthService(UserService userService) {
+    public AuthService(UserService userService, PasswordEncoder passwordEncoder) {
         super();
         this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public AuthResponse authenticate(Credentials credentials) {
-        // to do
+        User inDB = userService.getByUsername(credentials.getUsername());
+        boolean matches = passwordEncoder.matches(credentials.getPassword(), inDB.getPassword());
+
+        if(matches) {
+            UserVM userVM = new UserVM(inDB);
+            String token = Jwts.builder().setSubject("" + inDB.getId()).signWith(SignatureAlgorithm.HS512, "my-app-secret").compact();
+            AuthResponse response = new AuthResponse();
+            response.setUser(userVM);
+            response.setToken(token);
+            return response;
+        }
+
         return null;
     }
 }
