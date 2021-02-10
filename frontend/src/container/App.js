741b091e4af380d3567b8702471d8e9975c007ba
mustafakaplan <mstfakapln@gmail.com>
2021-02-10 13:36:37 +0300

    user page

--- frontend/src/container/App.js
+++ frontend/src/container/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { HashRouter, Redirect, Route, Switch } from "react-router-dom";
 import LoginPage from "../pages/LoginPage";
 import UserSignupPage from "../pages/UserSignupPage";
+import UserPage from "../pages/UserSignupPage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Switch>
         <Route path="/signup" component={UserSignupPage} />
         <Route path="/login" component={LoginPage} />
+        <Route path="/user/:username" component={UserPage} />
       </Switch>
     </HashRouter>
   );
---
+++ frontend/src/pages/UserPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const componentName = () => {
+  return <div>USER PAGE</div>;
+};
+
+export default componentName;
