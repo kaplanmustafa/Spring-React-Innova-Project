86076853f45da7a96fd4982ddd7d096acb1dbf00
mustafakaplan <mstfakapln@gmail.com>
2021-02-10 10:16:38 +0300

    login page layout and interactions

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import ButtonWithProgress from "../components/toolbox/ButtonWithProgress";
+import Input from "../components/toolbox/Input";
 
 const LoginPage = () => {
-  return <div>Login Page</div>;
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+
+  return (
+    <div className="container w-25 mt-5">
+      <form>
+        <h1 className="text-center mb-4">Login</h1>
+        <Input
+          label={"Username"}
+          onChange={(event) => {
+            setUsername(event.target.value);
+          }}
+        />
+        <Input
+          label={"Password"}
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
+          type="password"
+        />
+        <div className="text-center">
+          <ButtonWithProgress text={"Login"}></ButtonWithProgress>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
