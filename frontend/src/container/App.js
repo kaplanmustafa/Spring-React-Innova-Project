99fc6404187a274d4e1a3de0ca6e84282d9b49d8
mustafakaplan <mstfakapln@gmail.com>
2021-02-10 16:42:13 +0300

    404 error page

--- frontend/src/container/App.js
+++ frontend/src/container/App.js
@@ -1,16 +1,25 @@
 import React from "react";
-import { HashRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, Redirect, Route, Switch } from "react-router-dom";
 import LoginPage from "../pages/LoginPage";
 import UserSignupPage from "../pages/UserSignupPage";
 import UserPage from "../pages/UserPage";
+import ErrorPage from "../pages/ErrorPage";
+import { useSelector } from "react-redux";
 
 function App() {
+  const { isLoggedIn, role } = useSelector((store) => ({
+    isLoggedIn: store.isLoggedIn,
+    role: store.role,
+  }));
+
   return (
     <HashRouter>
       <Switch>
-        <Route path="/signup" component={UserSignupPage} />
-        <Route path="/login" component={LoginPage} />
-        <Route path="/user" component={UserPage} />
+        {!isLoggedIn && <Route path="/signup" component={UserSignupPage} />}
+        {!isLoggedIn && <Route path="/login" component={LoginPage} />}
+        {isLoggedIn && <Route path="/user" component={UserPage} />}
+        <Route path="/error" component={ErrorPage} />
+        <Redirect to="/error" />
       </Switch>
     </HashRouter>
   );
---
+++ frontend/src/pages/ErrorPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import errorImage from "../assets/error.png";
+
+const componentName = () => {
+  return (
+    <div className="container text-center">
+      <img src={errorImage} />
+    </div>
+  );
+};
+
+export default componentName;
