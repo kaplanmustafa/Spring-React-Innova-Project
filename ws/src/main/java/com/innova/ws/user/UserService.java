9856d404e854280058e5b474a541042c53696ad0
mustafakaplan <mstfakapln@gmail.com>
2021-02-10 11:01:55 +0300

    not found exception

--- ws/src/main/java/com/innova/ws/user/UserService.java
+++ ws/src/main/java/com/innova/ws/user/UserService.java
@@ -1,5 +1,6 @@
 package com.innova.ws.user;
 
+import com.innova.ws.error.NotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -18,5 +19,15 @@ public class UserService {
     public void save(User user) {
         user.setPassword(this.passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
+    }
+
+    public User getByUsername(String username) {
+        User inDB = userRepository.findByUsername(username);
+
+        if(inDB == null) {
+            throw new NotFoundException();
+        }
+
+        return inDB;
     }
 }
