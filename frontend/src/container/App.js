9becb8eb1022ee77b7bbce0b84884b4bd9500522
mustafakaplan <mstfakapln@gmail.com>
2021-02-10 10:07:52 +0300

    login page init

--- frontend/src/container/App.js
+++ frontend/src/container/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { HashRouter, Redirect, Route, Switch } from "react-router-dom";
+import LoginPage from "../pages/LoginPage";
 import UserSignupPage from "../pages/UserSignupPage";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <HashRouter>
       <Switch>
         <Route path="/signup" component={UserSignupPage} />
+        <Route path="/login" component={LoginPage} />
       </Switch>
     </HashRouter>
   );
