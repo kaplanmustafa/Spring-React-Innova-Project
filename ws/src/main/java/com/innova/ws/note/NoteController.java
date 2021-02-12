61597290276feb2cb6fe2543f253e18bec7dbf77
mustafakaplan <mstfakapln@gmail.com>
2021-02-12 12:27:07 +0300

    note service

--- ws/src/main/java/com/innova/ws/note/NoteController.java
+++ ws/src/main/java/com/innova/ws/note/NoteController.java
@@ -1,10 +1,26 @@
 package com.innova.ws.note;
 
+import com.innova.ws.shared.CurrentUser;
+import com.innova.ws.shared.GenericResponse;
+import com.innova.ws.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/1.0")
 public class NoteController {
-    
+
+    @Autowired
+    NoteService noteService;
+
+    @PostMapping("/notes")
+    GenericResponse saveNote(@Valid @RequestBody Note note, @CurrentUser User user) {
+        noteService.save(note, user);
+        return new GenericResponse("Note saved");
+    }
 }
---
+++ ws/src/main/java/com/innova/ws/note/NoteService.java
@@ -0,0 +1,12 @@
+package com.innova.ws.note;
+
+import com.innova.ws.user.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NoteService {
+
+
+    public void save(Note note, User user) {
+    }
+}
