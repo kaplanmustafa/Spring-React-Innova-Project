b857ee575a34ff1a249ac684059305c7528a27ec
mustafakaplan <mstfakapln@gmail.com>
2021-02-12 12:09:42 +0300

    note controller

---
+++ ws/src/main/java/com/innova/ws/note/NoteController.java
@@ -0,0 +1,10 @@
+package com.innova.ws.note;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/1.0")
+public class NoteController {
+    
+}
