3f331922f697ad4c42b65050acba85e659ba132d
mustafakaplan <mstfakapln@gmail.com>
2021-02-12 15:02:37 +0300

    note detail page layout

--- frontend/src/pages/NoteDetailPage.js
+++ frontend/src/pages/NoteDetailPage.js
@@ -1,7 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 
 const NoteDetailPage = () => {
-  return <div></div>;
+  const [inEditMode, setInEditMode] = useState(false);
+
+  return (
+    <div className="container">
+      <div className="container">
+        <form>
+          <div className="row">
+            <div className="col-10 form-group">
+              <label>Title</label>
+              <textarea
+                readOnly={!inEditMode}
+                className="form-control"
+                rows="1"
+              >
+                TITLE
+              </textarea>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-10 form-group">
+              <label>Your Note</label>
+              <textarea
+                readOnly={!inEditMode}
+                className="form-control"
+                rows="15"
+              >
+                CONTENT
+              </textarea>
+            </div>
+            {inEditMode && (
+              <div className="col-2">
+                <div className="row mt-5">
+                  <button className="btn btn-outline-success">Save</button>
+                </div>
+                <div className="row mt-3">
+                  <button
+                    className="btn btn-outline-danger"
+                    onClick={() => setInEditMode(false)}
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </div>
+            )}
+            {!inEditMode && (
+              <div className="col-2">
+                <div className="row mt-5">
+                  <button
+                    className="btn btn-outline-success"
+                    onClick={() => setInEditMode(true)}
+                  >
+                    Edit
+                  </button>
+                </div>
+                <div className="row mt-3">
+                  <button className="btn btn-outline-danger">Delete</button>
+                </div>
+              </div>
+            )}
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default NoteDetailPage;
