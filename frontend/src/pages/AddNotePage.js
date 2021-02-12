e0b19460e08f348234b7770e3d6d42d2bf4051cb
mustafakaplan <mstfakapln@gmail.com>
2021-02-12 15:11:08 +0300

    add note page layout

--- frontend/src/pages/AddNotePage.js
+++ frontend/src/pages/AddNotePage.js
@@ -1,7 +1,34 @@
 import React from "react";
 
 const AddNotePage = () => {
-  return <div>Add Note Page</div>;
+  return (
+    <div className="container">
+      <div className="container">
+        <form>
+          <div className="row">
+            <div className="col-10 form-group">
+              <label>Title</label>
+              <textarea className="form-control" rows="1"></textarea>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-10 form-group">
+              <label>Your Note</label>
+              <textarea className="form-control" rows="15"></textarea>
+            </div>
+            <div className="col-2">
+              <div className="row mt-5">
+                <button className="btn btn-outline-success">Save</button>
+              </div>
+              <div className="row mt-3">
+                <button className="btn btn-outline-danger">Cancel</button>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AddNotePage;
