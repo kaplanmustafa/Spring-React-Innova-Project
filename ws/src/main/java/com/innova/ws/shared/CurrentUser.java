a22689d5ef3d376fa0e43db6805a21df580862d8
mustafakaplan <mstfakapln@gmail.com>
2021-02-12 12:23:22 +0300

    current user interface

---
+++ ws/src/main/java/com/innova/ws/shared/CurrentUser.java
@@ -0,0 +1,16 @@
+package com.innova.ws.shared;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({ ElementType.PARAMETER })
+@Retention(RUNTIME)
+@AuthenticationPrincipal
+public @interface CurrentUser {
+
+}
