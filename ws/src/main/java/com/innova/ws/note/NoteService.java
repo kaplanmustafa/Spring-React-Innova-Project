51215c10e6903ab91cb7ec5f06b8618c0ea7a174
mustafakaplan <mstfakapln@gmail.com>
2021-02-12 12:34:27 +0300

    note repository

--- ws/src/main/java/com/innova/ws/note/NoteService.java
+++ ws/src/main/java/com/innova/ws/note/NoteService.java
@@ -3,10 +3,22 @@ package com.innova.ws.note;
 import com.innova.ws.user.User;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+
 @Service
 public class NoteService {
 
+    NoteRepository noteRepository;
+
+    public NoteService(NoteRepository noteRepository) {
+        this.noteRepository = noteRepository;
+    }
 
     public void save(Note note, User user) {
+        Note newNote = new Note();
+        newNote.setContent(note.getContent());
+        newNote.setTimestamp(new Date());
+        newNote.setUser(user);
+        noteRepository.save(newNote);
     }
 }
