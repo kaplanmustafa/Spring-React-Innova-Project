f53e687ba4961d9ee58786a6abfd17dd78a6b41e
mustafakaplan <mstfakapln@gmail.com>
2021-02-15 11:40:34 +0300

    comment controller

---
+++ ws/src/main/java/com/innova/ws/comment/CommentController.java
@@ -0,0 +1,22 @@
+package com.innova.ws.comment;
+
+import com.innova.ws.shared.CurrentUser;
+import com.innova.ws.shared.GenericResponse;
+import com.innova.ws.user.User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/1.0")
+public class CommentController {
+
+    @PostMapping("/notes")
+    GenericResponse saveNote(@Valid @RequestBody Comment comment, @CurrentUser User user) {
+        // to do
+        return new GenericResponse("Note saved");
+    }
+}
