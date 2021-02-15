a3a010c7a2591a317fb0bca4547031d57888c0ae
mustafakaplan <mstfakapln@gmail.com>
2021-02-15 12:03:15 +0300

    saving comment to database

--- ws/src/main/java/com/innova/ws/comment/CommentController.java
+++ ws/src/main/java/com/innova/ws/comment/CommentController.java
@@ -3,10 +3,8 @@ package com.innova.ws.comment;
 import com.innova.ws.shared.CurrentUser;
 import com.innova.ws.shared.GenericResponse;
 import com.innova.ws.user.User;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -14,9 +12,12 @@ import javax.validation.Valid;
 @RequestMapping("/api/1.0")
 public class CommentController {
 
-    @PostMapping("/notes")
-    GenericResponse saveComment(@Valid @RequestBody Comment comment, @CurrentUser User user) {
-        // to do
+    @Autowired
+    CommentService commentService;
+
+    @PostMapping("/comments/{noteId}")
+    GenericResponse saveComment(@Valid @RequestBody Comment comment, @PathVariable long noteId, @CurrentUser User user) {
+        commentService.save(comment, noteId, user);
         return new GenericResponse("Comment saved");
     }
 }
--- ws/src/main/java/com/innova/ws/comment/CommentService.java
+++ ws/src/main/java/com/innova/ws/comment/CommentService.java
@@ -1,16 +1,34 @@
 package com.innova.ws.comment;
 
-import com.innova.ws.user.UserService;
+import com.innova.ws.error.NotFoundException;
+import com.innova.ws.note.Note;
+import com.innova.ws.note.NoteService;
+import com.innova.ws.user.User;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class CommentService {
 
     CommentRepository commentRepository;
-    UserService userService;
+    NoteService noteService;
 
-    public CommentService(CommentRepository commentRepository, UserService userService) {
+    public CommentService(CommentRepository commentRepository, NoteService noteService) {
         this.commentRepository = commentRepository;
-        this.userService = userService;
+        this.noteService = noteService;
+    }
+
+    public void save(Comment comment, long noteId, User user) {
+        Note note = noteService.getNoteByNoteId(noteId);
+        if(note == null) {
+            throw new NotFoundException();
+        }
+
+        Comment newComment = new Comment();
+        newComment.setComment(comment.getComment());
+        newComment.setTimestamp(new Date());
+        newComment.setNote(note);
+        commentRepository.save(newComment);
     }
 }
