1aa0bddd8789c427baf772966a111a32b3f14b9c
mustafakaplan <mstfakapln@gmail.com>
2021-02-24 14:08:06 +0300

    refactor initial user

--- ws/src/main/java/com/innova/ws/WsApplication.java
+++ ws/src/main/java/com/innova/ws/WsApplication.java
@@ -1,12 +1,35 @@
 package com.innova.ws;
 
+import com.innova.ws.user.User;
+import com.innova.ws.user.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class WsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WsApplication.class, args);
+	}
+
+	@Bean
+	@Profile("dev")
+	CommandLineRunner createInitialUsers(UserService userService) {
+		return(args) -> {
+			try {
+				userService.getByUsername("user1");
+			} catch (Exception e) {
+				for(int i=1; i<=25; i++) {
+					User user = new User();
+					user.setUsername("user" + i);
+					user.setFullName("name" + i);
+					user.setPassword("P4ssword");
+					userService.save(user, "user");
+				}
+			}
+		};
 	}
 }
